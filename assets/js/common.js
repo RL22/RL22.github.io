a123229af60934651440f808ee820b303193f9a3
Rodney Lewis <lewis.rodneyl@gmail.com>
2016-10-02 16:18:51 -0700

    img positioning fixed

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-     // // Typing text
-     // $(".animated-text").typed({
-     //     strings: [
-     //         "I'm a web designer & developer based in San Francisco, CA",
-     //          "with a passion for simple, functional & user oriented design."
-     //     ],
-     //     typeSpeed: 40,
-     //     loop: true
-     // });
