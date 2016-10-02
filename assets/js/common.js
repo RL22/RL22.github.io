28d9d5faae7d69bce09271e4b87610aec3ea6415
Rodney Lewis <lewis.rodneyl@gmail.com>
2016-10-01 21:34:31 -0700

    Re-design Complete

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+     // // Typing text
+     // $(".animated-text").typed({
+     //     strings: [
+     //         "I'm a web designer & developer based in San Francisco, CA",
+     //          "with a passion for simple, functional & user oriented design."
+     //     ],
+     //     typeSpeed: 40,
+     //     loop: true
+     // });
